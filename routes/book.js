f737315b8a6e553f62b3fd50d384e200d9b507b5
PxS00 <lucas.rossonidd@gmail.com>
2025-07-14 17:50:29 -0300

    feat: Add error handling to GET route and implement POST, PATCH, and DELETE routes in book.js

--- routes/book.js
+++ routes/book.js
@@ -3,7 +3,23 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.send('Hello World')
+    try {
+        res.send('Hello World')
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
 })
 
+router.post('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo POST')
+})
+
+router.patch('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo PATCH')
+})
+
+router.delete('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo DELETE')
+})
 module.exports = router
