d97b2956494fc126e31fac0f386f7ae4b04cde2c
PxS00 <lucas.rossonidd@gmail.com>
2025-07-15 15:40:00 -0300

    feat: Add getBook function to retrieve a book by ID and update routes accordingly

--- controllers/book.js
+++ controllers/book.js
@@ -1,4 +1,4 @@
-const { getAllBooks } = require('../service/book')
+const { getAllBooks, getBooksId } = require('../service/book')
 
 function getBooks(req, res) {
     try {
@@ -10,6 +10,19 @@ function getBooks(req, res) {
     }
 }
 
+function getBook(req, res) {
+    try {
+        const id = req.params.id
+        const book = getBooksId(id)
+        res.send(book)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+
 module.exports = {
-    getBooks
+    getBooks,
+    getBook
 }
--- service/book.js
+++ service/book.js
@@ -3,6 +3,15 @@ function getAllBooks(){
     return JSON.parse(fs.readFileSync('books.json'))
 }
 
+function getBooksId(id){
+    const books = JSON.parse(fs.readFileSync('books.json'))
+
+    const filteredBook = books.filter(book => book.id === id) [0]
+    return filteredBook
+    
+}
+
 module.exports = {
-    getAllBooks
+    getAllBooks,
+    getBooksId
 }
