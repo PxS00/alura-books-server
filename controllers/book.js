c5e70e3d4e538721683201f307b889b6bca41559
PxS00 <lucas.rossonidd@gmail.com>
2025-07-15 18:01:59 -0300

    feat: Add new book to JSON data and enhance error handling in book controller functions

--- controllers/book.js
+++ controllers/book.js
@@ -2,65 +2,99 @@ const { getAllBooks, getBooksId, insertBook, modifyBook, deleteBookId} = require
 
 function getBooks(req, res) {
   try {
-        const books = getAllBooks();
-        res.send(books);
+    const books = getAllBooks();
+    res.send(books);
   } catch (error) {
-        res.status(500);
-        res.send(error.message);
+    res.status(500);
+    res.send(error.message);
   }
 }
 
 function getBook(req, res) {
   try {
-        const id = req.params.id;
-        const book = getBooksId(id);
+    const id = req.params.id;
+
+    if (id && Number(id)) {
+      const book = getBooksId(id);
+      if (book) {
         res.send(book);
+      } else {
+        res.status(404);
+        res.send("Livro não encontrado");
+      }
+    } else {
+      res.status(422);
+      res.send("Id inválido");
+    }
   } catch (error) {
-        res.status(500);
-        res.send(error.message);
+    res.status(500);
+    res.send(error.message);
   }
 }
 
 function postBook(req, res) {
   try {
-        const newBook = req.body;
-        insertBook(newBook);
-        res.status(201);
-        res.send("Livro inserido com sucesso");
+    const newBook = req.body;
+    insertBook(newBook);
+    res.status(201);
+    res.send("Livro inserido com sucesso");
   } catch (error) {
-        res.status(500);
-        res.send(error.message);
+    res.status(500);
+    res.send(error.message);
   }
 }
 
-function patchBook(req, res){
-    try {
-        const id = req.params.id
-        const body = req.body
+function patchBook(req, res) {
+  try {
+    const id = req.params.id;
 
-        modifyBook(body, id)
-        res.send('Item modificado com sucesso')
-        
-    } catch (error) {
-        res.status(500);
-        res.send(error.message);  
+    if (id && Number(id)) {
+      const book = getBooksId(id);
+      if (book) {
+        const body = req.body;
+        modifyBook(body, id);
+        res.send("Item modificado com sucesso");
+      } else {
+        res.status(404);
+        res.send("Livro não encontrado");
+      }
+    } else {
+      res.status(422);
+      res.send("Id inválido");
     }
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
 }
 
-function deleteBook(req, res){
-    try {
-        const id = req.params.id
-        deleteBookId(id)
-        res.send('Item deletado com sucesso')
-    } catch (error) {
-        res.status(500);
-        res.send(error.message); 
+function deleteBook(req, res) {
+  try {
+    const id = req.params.id;
+
+    if (id && Number(id)) {
+      const book = getBooksId(id);
+      if (book) {
+        deleteBookId(id);
+        res.send("Item deletado com sucesso");
+      } else {
+        res.status(404);
+        res.send("Livro não encontrado");
+      }
+    } else {
+      res.status(422);
+      res.send("Id inválido");
     }
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
 }
+
 module.exports = {
   getBooks,
   getBook,
   postBook,
   patchBook,
-  deleteBook
+  deleteBook,
 };
